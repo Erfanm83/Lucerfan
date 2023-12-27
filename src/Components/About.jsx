7b5f2a5e3286fb2d577393537efdd3b118f5e0e2
Erfan Mahmoudi <56364018+Erfanm83@users.noreply.github.com>
2023-12-27 14:27:01 +0000

    url links added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "a man standing in front of a laptop";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a CE student studying at Ferdowsi University of Mashhad. I enjoy creating unique web pages in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Backend Development",
+  "Software Development",
+  "Web Development",
+  "SQL Database",
+  "Unit Testing",
+  "Robotics Programming",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Backend_development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
